f28d3781eda6cf58ee9ec2eee1f2c7bc2e43e616
Siddhant Singh <sidsingh250496@gmail.com>
2024-04-25 14:16:04 +0530

    First commit

---
+++ src/main/java/com/example/gitcommitiddemo/GitCommitIdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitcommitiddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommitIdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCommitIdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitcommitiddemo/GitCommitIdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitcommitiddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommitIdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
